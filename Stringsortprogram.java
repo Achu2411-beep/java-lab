629b8e0b0dc56b60df2ee6eecc7869b1d1c70f7c
Aswathivijayachandran <aswathivijayachandran@gmail.com>
2026-02-27 10:23:58 +0530

    done

---
+++ Stringsortprogram.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Stringsortprogram {
+
+    
+    static void userDefinedSort(String arr[], int n) {
+        for(int i = 0; i < n-1; i++) {
+            for(int j = 0; j < n-i-1; j++) {
+                if(arr[j].compareTo(arr[j+1]) > 0) {
+                    String temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of strings: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String arr[] = new String[n];
+
+        System.out.println("Enter the strings:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextLine();
+        }
+
+        System.out.println("\nMenu");
+        System.out.println("1. Built-in Sorting");
+        System.out.println("2. User-defined Sorting");
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
+
+        switch(choice) {
+            case 1:
+                Arrays.sort(arr);  
+                System.out.println("\nSorted using Built-in method:");
+                break;
+
+            case 2:
+                userDefinedSort(arr, n);  
+                System.out.println("\nSorted using User-defined method:");
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+                return;
+        }
+
+        for(String s : arr) {
+            System.out.println(s);
+        }
+
+        sc.close();
+    }
+}
